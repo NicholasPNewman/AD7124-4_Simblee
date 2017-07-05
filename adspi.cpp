7a1e92b246e94b4638ea1041ace0877e172117fb
Nicholas Newman <nicknewman@att.net>
2017-07-05 17:00:14 -0400

    first file add, includes old library contents

---
+++ adspi.cpp
@@ -0,0 +1,171 @@
+#include "adspi.h"
+
+adspiClass adspi;
+
+int adspiClass::comm(int command, int value) {
+  uint8_t dat_in;
+  
+  // take the SS pin low to select the chip:
+  digitalWrite(ADSPI_CS, LOW);
+  //  send in the address and value via SPI:
+  SPI.transfer(command);
+  dat_in = (SPI.transfer(value));
+  Serial.println(dat_in);
+  // take the SS pin high to de-select the chip:
+  digitalWrite(ADSPI_CS, HIGH); 
+
+  return dat_in;
+}
+
+// @brief: reads ID register and returns 1 for correct response and 0 for incorrect
+int adspiClass::verify() {
+  uint8_t dat_in;
+  uint8_t dat_valid = 4;
+  digitalWrite(ADSPI_CS, LOW);
+  SPI.transfer(COMM_R_ID);
+  if (SPI.transfer(0) == dat_valid) {
+    digitalWrite(ADSPI_CS, HIGH);
+    return 1;
+  }
+  else {
+    digitalWrite(ADSPI_CS, HIGH);
+    return 0;
+  }
+}
+
+// @brief: returns the contents of the status register
+uint8_t adspiClass::status() {
+  uint8_t dat_in;
+  digitalWrite(ADSPI_CS, LOW);
+  SPI.transfer(COMM_R_STAT);
+  dat_in = SPI.transfer(0);
+  digitalWrite(ADSPI_CS, HIGH);
+  return dat_in;
+}
+
+// @brief: returns the contents of the error register
+uint8_t adspiClass::err() {
+  uint8_t dat_in;
+  digitalWrite(ADSPI_CS, LOW);
+  SPI.transfer(COMM_R_ERR);
+  dat_in = SPI.transfer(0);
+  digitalWrite(ADSPI_CS, HIGH);
+  return dat_in;  
+}
+
+// @breif: returns the contents of the config register (16 bit)
+uint16_t adspiClass::getconfig(int setup_n){
+  if ((setup_n < 0) || (setup_n > 7)) {
+    return 0;
+  }
+  uint8_t dat_in1;
+  uint8_t dat_in2;
+  uint16_t dat_16;
+  digitalWrite(ADSPI_CS, LOW);
+  SPI.transfer(COMM_R_CONFIG + setup_n);
+  dat_in1 = SPI.transfer(0);
+  dat_in2 = SPI.transfer(0);
+  digitalWrite(ADSPI_CS, HIGH);
+  dat_16 = (dat_in1 << 8) + dat_in2;
+  return dat_16;  
+}
+
+// @breif: returns the contents of the filter register (24 bit)
+int adspiClass::getfilter(int setup_n) {
+  if ((setup_n < 0) || (setup_n > 7)) {
+    return 0;
+  }
+  uint8_t dat_in1;
+  uint8_t dat_in2;
+  uint8_t dat_in3;
+  int dat_24;
+  digitalWrite(ADSPI_CS, LOW);
+  SPI.transfer(COMM_R_CONFIG + setup_n);
+  dat_in1 = SPI.transfer(0);
+  dat_in2 = SPI.transfer(0);
+  dat_in3 = SPI.transfer(0);
+  digitalWrite(ADSPI_CS, HIGH);
+  dat_24 = (dat_in1 << 16) + (dat_in2 << 8) + dat_in3;
+  return dat_24;  
+}
+
+// @breif: request data and read 24-bit converted value
+int adspiClass::data() {
+  uint8_t dat_in1;
+  uint8_t dat_in2;
+  uint8_t dat_in3;
+  int dat_24;
+  digitalWrite(ADSPI_CS, LOW);
+  SPI.transfer(COMM_R_DATA);
+  dat_in1 = SPI.transfer(0);
+  dat_in2 = SPI.transfer(0);
+  dat_in3 = SPI.transfer(0);
+  digitalWrite(ADSPI_CS, HIGH);
+  dat_24 = (dat_in1 << 16) + (dat_in2 << 8) + dat_in3;
+  return dat_24;  
+}
+// @brief: configure an individual channel
+void adspiClass::channel_cfg(int channel_n, int c_en, int c_setup, int c_ainp, int c_ainm) {
+  uint16_t ch_config;
+  uint8_t  config_1;
+  uint8_t  config_2;
+
+  ch_config     = ((c_en     << CHN_ENABLE) ||    // 1 bit (enable the channel (max 4))
+                   (c_setup  << CHN_SETUP)  ||    // 3 bit (choose the associated setup (0))
+                   (c_ainp   << CHN_AINP)   ||    // 5 bit (choose AINP input)
+                   (c_ainm   << CHN_AINM));       // 5 bit (choose AINM input)
+
+  // split new config into two parts for sending
+  config_1 = (ch_config >> 8);
+  config_2 = (ch_config & (0xFF));
+
+  // command and send
+  digitalWrite(ADSPI_CS, LOW);
+  SPI.transfer(COMM_W_CHANNEL + channel_n);   // write to channel register base address plus offset
+  SPI.transfer(config_1);
+  SPI.transfer(config_2);
+  digitalWrite(ADSPI_CS, HIGH);
+
+  Serial.println("Channels configured.");
+  
+}
+
+// @brief: configure an individual setup
+void adspiClass::setup_cfg(int setup_n) {
+  Serial.println("Setups configured.");
+}
+
+// @brief: configure system diagnostics
+void adspiClass::diag_cfg(int config_n) {
+  Serial.println("Diagnostics configured.");
+}
+
+// @brief: configure  ADC control
+void adspiClass::control_cfg(int clk_sel, int mode, int power, int ref_en, int cs_en, int data_status,
+                       int cont_read, int dout_rdy) {
+  uint16_t ctrl_config;
+  uint8_t  config_1;
+  uint8_t  config_2;
+
+  ctrl_config     = ((clk_sel     << CTRL_CLK_SEL)      ||    // 2 bit (11 external clk / 4) (other -> internal clk)
+                     (mode        << CTRL_MODE)         ||    // 4 bit (see data sheet for mode options)
+                     (power       << CTRL_POWER)        ||    // 2 bit (00 low) (01 mid) (10,11 full)
+                     (ref_en      << CTRL_REF_EN)       ||    // 1 bit (allow interal reference access through REFOUT pin)
+                     (cs_en       << CTRL_CS_EN)        ||    // 1 bit (keep 0 for normal op, see datasheet for 1)
+                     (data_status << CTRL_DATA_STATUS)  ||    // 1 bit (enabled sets transmission of status register after data)
+                     (cont_read   << CTRL_CONT_READ)    ||
+                     (dout_rdy    << CTRL_DOUT));
+
+  // split config into two parts for sending   
+  config_1 = (ctrl_config >> 8);
+  config_2 = (ctrl_config & (0xFF));
+
+  // command and send
+  digitalWrite(ADSPI_CS, LOW);
+  SPI.transfer(COMM_W_CTRL);
+  SPI.transfer(config_1);
+  SPI.transfer(config_2);
+  digitalWrite(ADSPI_CS, HIGH);
+
+  Serial.println("Controls configured.");
+}
---
+++ adspi.h
@@ -0,0 +1,70 @@
+#ifndef adspi_h
+#define adspi_h
+
+#include <Arduino.h>
+#include "SPI.h"
+
+#define ADSPI_CS              (21)
+
+// read commands
+#define COMM_R_STAT         (0x40)
+#define COMM_R_CTRL         (0x41)
+#define COMM_R_DATA         (0x42)
+#define COMM_R_IO_CTRL1     (0x43)
+#define COMM_R_IO_CTRL2     (0x44)
+#define COMM_R_ID           (0x45)
+#define COMM_R_ERR          (0x46)
+#define COMM_R_ERR_EN       (0x47)
+#define COMM_R_MCOUNT       (0x48)
+#define COMM_R_CHANNEL      (0x49)
+#define COMM_R_CONFIG       (0x59)
+#define COMM_R_FILTER       (0x61)
+
+// write commands
+#define COMM_W_CTRL         (0x01)
+#define COMM_W_IO_CTRL1     (0x03)
+#define COMM_W_IO_CTRL2     (0x04)
+#define COMM_W_ERR_EN       (0x07)
+#define COMM_W_CHANNEL      (0x09)
+#define COMM_W_CONFIG       (0x19)
+#define COMM_W_FILTER       (0x21)
+#define COMM_W_OFFSET       (0x29)
+#define COMM_W_GAIN         (0x31)
+
+// ADC control definitions / register locations
+#define CTRL_CLK_SEL        (0x00)
+#define CTRL_MODE           (0x02)
+#define CTRL_POWER          (0x06)
+#define CTRL_REF_EN         (0x08)
+#define CTRL_CS_EN          (0x09)
+#define CTRL_DATA_STATUS    (0x0A)
+#define CTRL_CONT_READ      (0x0B)
+#define CTRL_DOUT           (0x0C)
+
+// channel definitions / register locations
+#define CHN_AINM            (0x00)
+#define CHN_AINP            (0x05)
+#define CHN_SETUP           (0x0C)
+#define CHN_ENABLE          (0x0F)
+
+// function definitions
+
+class adspiClass {
+public:
+  int       verify();
+  
+  int       comm(int command, int value); 
+  uint8_t   status();
+  uint8_t   err();
+  uint16_t  getconfig(int setup_n);
+  int       getfilter(int setup_n);
+  int       data();
+  void      channel_cfg(int channel_n, int c_en, int c_setup, int c_ainp, int c_ainm);
+  void      setup_cfg(int setup_n);
+  void      diag_cfg(int config_n);
+  void      control_cfg(int clk_sel, int mode, int power, int ref_en, int cs_en, int data_status, int cont_read, int dout_rdy);
+};
+
+extern adspiClass adspi;
+
+#endif
